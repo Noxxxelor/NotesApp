d5bcd732e77f099727bfde1add63e8d5a4879625
Noxxxelor <stu2301321031@uni-plovdiv.bg>
2025-12-07 12:46:11 +0200

    Change date show in notes

--- app/src/main/java/com/example/notesapp/ui/notes/NoteAdapter.kt
+++ app/src/main/java/com/example/notesapp/ui/notes/NoteAdapter.kt
@@ -7,16 +7,21 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesapp.data.model.Note
 import com.example.notesapp.databinding.ItemNoteBinding
-
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteViewHolder>(DiffCallback()) {
 
-    var onItemClick: ((Note) -> Unit)? = null  // <- добавляем callback
+    var onItemClick: ((Note) -> Unit)? = null
 
     inner class NoteViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+
         fun bind(note: Note) {
             binding.textTitle.text = note.title.ifBlank { "Без заголовка" }
             binding.textContent.text = note.content
+            binding.textDate.text = dateFormat.format(Date(note.timestamp))
 
             binding.root.setOnClickListener {
                 onItemClick?.invoke(note)
